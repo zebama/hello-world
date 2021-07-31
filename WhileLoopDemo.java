51492c8280c8df82e53816169f7c24d2ed3a3494
zebama <86637199+zebama@users.noreply.github.com>
2021-07-31 21:10:34 +0530

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WhileLoopDemo {
+	public static void main(String[] args) {
+		
+		int number, sum=0;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("n please Enter any integer value below 10: ");
+		
+		number = sc.nextInt();
+		
+		while (number <=10) {
+			sum=sum+number;
+			number++;
+			
+			System.out.format(" sum of the numbers from the while loop is: %d ", sum);
+			
+			
+			
+		}
+		
+				
+	}
+
+}
