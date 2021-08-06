68770d61864815a8104412343515f6f114d869f9
zebama <86637199+zebama@users.noreply.github.com>
2021-08-06 22:34:01 +0530

    Add files via upload

---
+++ ReversingANumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReversingANumber {
+public static void main(String[] args) {
+	
+	int num=0;
+	int reversenum = 0;
+	System.out.println("Input your number and press enter: ");
+	// this statement will capture the user input
+	Scanner in = new Scanner(System.in);
+	//capture input would be stored in number num
+	num = in.nextInt();
+	//while loop: logic to find out the reverse number
+	while(num !=0 )
+	{
+		reversenum = reversenum * 10;
+		reversenum = reversenum + num%10;
+		num = num/10;
+		
+		System.out.println("Reverse of input number is: " + reversenum);
+		
+	}
+	
+}
+}
